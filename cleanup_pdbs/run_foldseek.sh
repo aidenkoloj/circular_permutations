6cb19532d1fd67b12513b72457be084fd02af032
Aiden Kolodziej <aidenkoloj@gmail.com>
2024-05-18 19:13:03 -0400

    Edited run_foldseek.sh to generate a log of files it has processed with a timestamp

--- cleanup_pdbs/run_foldseek.sh
+++ cleanup_pdbs/run_foldseek.sh
@@ -14,6 +14,19 @@
 
 # Sbatch script to run foldseek on each file in each subdirectory
 
+# Define a log file containing files that have been processed
+
+
+log="log_run_foldseek.txt"
+
+if [ -e "$log" ]; then
+    echo "$log already exists. Appending to log"
+else
+    touch "$log"
+    echo "Created $log"
+fi
+
+
 
 # Define parent dir as the working dir
 parent_directory=$(pwd)
@@ -24,7 +37,7 @@ for subdirectory in "$parent_directory"/*cp; do # loop through subdirectories en
     if [ -d "$subdirectory" ]; then  # Check if it's a directory
         echo "Processing files in directory: $subdirectory"
         # Loop through each file in the subdirectory
-        for input_file in "$subdirectory"/*; do
+        for input_file in "$subdirectory"/*txt; do
             if [ -f "$input_file" ]; then  # Check if it's a regular file
                 echo "Processing file: $file"
                 
@@ -45,7 +58,9 @@ for subdirectory in "$parent_directory"/*cp; do # loop through subdirectories en
 # Before we run foldseek, let's check that the output file doesnt already exist (we may have already run foldseek in a previous sbatch job 
                 if [ ! -f "$output_file" ]; then
                     echo "Output file doesn't exist, running foldseek command..."
-                    foldseek easy-search "$input_file" AFproteome "$output_dir/$(basename "${input_file%.txt}.aln1.m8")" foldseek.tmp --alignment-type 1 --tmscore-threshold 0.0 -e 10 --format-output "query,target,mismatch,gapopen,pident,qstart,qend,qlen,tstart,tend,tlen,alnlen,evalue,alntmscore,qtmscore,ttmscore,qaln,taln,cigar,qseq,tseq"        
+                    foldseek easy-search "$input_file" AFproteome "$output_dir/$(basename "${input_file%.txt}.aln1.m8")" foldseek.tmp --alignment-type 1 --tmscore-threshold 0.0 -e 10 --format-output "query,target,mismatch,gapopen,pident,qstart,qend,qlen,tstart,tend,tlen,alnlen,evalue,alntmscore,qtmscore,ttmscore,qaln,taln,cigar,qseq,tseq"
+                    current_time=$(date +"%Y-%m-%d %H:%M:%S")
+                    echo -e "Foldseek output generated for: $output_file\t$current_time" >> "$log"
                 else
                     echo "Output file already exists: $output_file, skipping foldseek command."
                 fi
